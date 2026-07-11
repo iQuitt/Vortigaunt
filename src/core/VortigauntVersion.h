a1556f431d6ec44a6863caca155d48dc16fdb207
iQuit <XecT@hotmail.com>
2026-07-11 17:34:27 +0300

    Update the Version of Vortigaunt tool

--- src/core/VortigauntVersion.h
+++ src/core/VortigauntVersion.h
@@ -2,17 +2,17 @@
 
 // cmake will generate VortigauntVersion.h from this file
 #define VORTIGAUNT_VERSION_MAJOR 1
-#define VORTIGAUNT_VERSION_MINOR 0
-#define VORTIGAUNT_VERSION_PATCH 1
+#define VORTIGAUNT_VERSION_MINOR 1
+#define VORTIGAUNT_VERSION_PATCH 0
 
-#define VORTIGAUNT_VERSION_STRING "1.0.1"
+#define VORTIGAUNT_VERSION_STRING "1.1.0"
 
 // Build info (populated by CMake at configure time)
-#define VORTIGAUNT_GIT_COMMIT "eeca882"
+#define VORTIGAUNT_GIT_COMMIT "b2226c1"
 #define VORTIGAUNT_GIT_BRANCH "master"
 #define VORTIGAUNT_BUILD_TYPE "Release"
 #define VORTIGAUNT_OS "Windows"
-#define VORTIGAUNT_LINES_OF_CODE 43676
+#define VORTIGAUNT_LINES_OF_CODE 43717
 
 // GitHub repository for update checks
 #define VORTIGAUNT_GITHUB_OWNER "iQuitt"
